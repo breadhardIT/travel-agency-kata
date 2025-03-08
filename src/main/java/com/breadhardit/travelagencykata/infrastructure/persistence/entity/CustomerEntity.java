8ef10c63d59b67f4ef07ecfe8b3ffa2867db10e5
pablo-mbr <pablomaciasbrad@gmail.com>
2025-03-08 15:49:09 +0100

    Constructores para CustomerEntity

--- src/main/java/com/breadhardit/travelagencykata/infrastructure/persistence/entity/CustomerEntity.java
+++ src/main/java/com/breadhardit/travelagencykata/infrastructure/persistence/entity/CustomerEntity.java
@@ -4,14 +4,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "CUSTOMERS")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CustomerEntity {
     @Id
