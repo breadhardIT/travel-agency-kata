82fb948a1857b2b955eaa5316952f817aa9c6957
juanlu-ms <juanlums11@gmail.com>
2025-03-10 03:06:21 +0100

    implement CustomerRepositoryAdapter for customer persistence

---
+++ src/main/java/com/breadhardit/travelagencykata/infrastructure/persistence/repository/CustomerRepositoryAdapter.java
@@ -0,0 +1,66 @@
+package com.breadhardit.travelagencykata.infrastructure.persistence.repository;
+
+import com.breadhardit.travelagencykata.application.port.CustomersRepository;
+import com.breadhardit.travelagencykata.domain.Customer;
+import com.breadhardit.travelagencykata.infrastructure.persistence.entity.CustomerEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+@Primary
+@Scope("singleton")
+public class CustomerRepositoryAdapter implements CustomersRepository {
+
+    @Autowired
+    private CustomersJPARepository customersJPARepository;
+
+    @Override
+    public void saveCustomer(Customer customer) {
+        CustomerEntity customerEntity = CustomerEntity.builder()
+                .id(customer.getId())
+                .name(customer.getName())
+                .surnames(customer.getSurnames())
+                .birthDate(customer.getBirthDate())
+                .passportNumber(customer.getPassportNumber())
+                .enrollmentDate(LocalDate.now())
+                .active(Boolean.TRUE)
+                .build();
+        customersJPARepository.save(customerEntity);
+    }
+
+    @Override
+    public Optional<Customer> getCustomerById(String id) {
+        CustomerEntity customerEntity = customersJPARepository.findById(id).orElse(null);
+        return customerEntity == null ? Optional.empty()
+                : Optional.of(Customer.builder()
+                .id(customerEntity.getId())
+                .name(customerEntity.getName())
+                .surnames(customerEntity.getSurnames())
+                .birthDate(customerEntity.getBirthDate())
+                .passportNumber(customerEntity.getPassportNumber())
+                .enrollmentDate(customerEntity.getEnrollmentDate())
+                .active(customerEntity.getActive())
+                .build());
+    }
+
+    @Override
+    public Optional<Customer> getCustomerByPassport(String id) {
+        CustomerEntity customerEntity = customersJPARepository.findAll().stream()
+                .filter(c -> c.getPassportNumber().equals(id)).findFirst().orElse(null);
+        return customerEntity == null ? Optional.empty()
+                : Optional.of(Customer.builder()
+                .id(customerEntity.getId())
+                .name(customerEntity.getName())
+                .surnames(customerEntity.getSurnames())
+                .birthDate(customerEntity.getBirthDate())
+                .passportNumber(customerEntity.getPassportNumber())
+                .enrollmentDate(customerEntity.getEnrollmentDate())
+                .active(customerEntity.getActive())
+                .build());
+    }
+}
