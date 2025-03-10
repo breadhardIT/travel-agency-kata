987b0e7de70facab80d943df3c8916c292a7bee2
alberto <alberto.ortizort@alumnos.upm.es>
2025-03-10 10:45:21 +0100

    Correciones

--- src/main/java/com/breadhardit/travelagencykata/infrastructure/persistence/entity/CustomerEntity.java
+++ src/main/java/com/breadhardit/travelagencykata/infrastructure/persistence/entity/CustomerEntity.java
@@ -1,10 +1,14 @@
 package com.breadhardit.travelagencykata.infrastructure.persistence.entity;
 
-import com.breadhardit.travelagencykata.domain.Customer;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 @Entity
@@ -12,55 +16,22 @@ import java.time.LocalDate;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomerEntity {
     @Id
     @Column(name = "ID")
     String id;
-
     @Column(name = "NAME")
     String name;
-
     @Column(name = "SURNAMES")
     String surnames;
-
     @Column(name = "BIRTH_DATE")
     LocalDate birthDate;
-
     @Column(name = "PASSPORT_NUMBER")
     String passportNumber;
-
     @Column(name = "ENROLLMENT_DATE")
     LocalDate enrollmentDate;
-
     @Column(name = "ACTIVE")
     Boolean active;
-
-    public CustomerEntity() {
-
-    }
-
-    public static CustomerEntity fromDomain(Customer customer) {
-        return CustomerEntity.builder()
-                .id(customer.getId())
-                .name(customer.getName())
-                .surnames(customer.getSurnames())
-                .birthDate(customer.getBirthDate())
-                .passportNumber(customer.getPassportNumber())
-                .enrollmentDate(customer.getEnrollmentDate())
-                .active(customer.getActive())
-                .build();
-    }
-
-    public Customer toDomain() {
-        return Customer.builder()
-                .id(id)
-                .name(name)
-                .surnames(surnames)
-                .birthDate(birthDate)
-                .passportNumber(passportNumber)
-                .enrollmentDate(enrollmentDate)
-                .active(active)
-                .build();
-    }
 }
 
